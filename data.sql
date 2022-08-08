3c84c499876818f166536bdf46b3f8bb553be7dc
maen <maenkh@yahoo.com>
2022-08-08 17:54:41 +0300

    create database vet_clinin and table animals , insert data, queries on the inserted data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', '2/3/2020', 0, '1', 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon','11/15/2018', 2, '1', 8);
+INSERT INTO animals VALUES (3, 'Pikachu','7/1/2021', 1, '0', 15.04);
+INSERT INTO animals VALUES (4, 'Devimon','5/12/2018', 5, '1', 11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals  where name like '%mon';
+select name from animals where date_of_birth between '1/1/2016' and '1/1/2019';
+select name from animals where neutered = '1' and escape_attempts < 3;
+select date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
+select name,escape_attempts from animals where weight_kg > 10.5;
+select * from animals where neutered = '1';
+select * from animals where name != 'Gabumon';
+select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
